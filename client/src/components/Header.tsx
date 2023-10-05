562c44fe0a45a8ccc2b76d6d20b92de8298a62d8
Tim Silva <tim17silva@gmail.com>
2023-10-05 15:57:54 -0700

    Added react icons

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { BsHandbag } from "react-icons/bs";
+
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-center h-24 bg-red-200">
@@ -6,6 +8,7 @@ export default function Header() {
         <span>Men</span>
         <span>Women</span>
       </div>
+      <BsHandbag />
     </header>
   );
 }
