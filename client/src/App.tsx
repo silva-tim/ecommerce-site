01c9cdd4d2ab762f8bd22fdbf5aac5a0cc55e3e2
Tim Silva <tim17silva@gmail.com>
2023-10-06 17:58:45 -0700

    Fixing header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Header />} />
+      <Route path="/" element={<Header />}></Route>
     </Routes>
   );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,16 @@ import { BsHandbag } from "react-icons/bs";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-center h-24 bg-red-200">
-      <span className="basis-1/3">Thesis</span>
-      <div className="basis-1/3">
+      <div className="flex justify-center basis-1/5">
+        <span>Thesis</span>
+      </div>
+      <div className="flex justify-center gap-6 basis-3/5">
         <span>Men</span>
         <span>Women</span>
       </div>
-      <BsHandbag />
+      <div className="flex justify-center basis-1/5">
+        <BsHandbag />
+      </div>
     </header>
   );
 }
