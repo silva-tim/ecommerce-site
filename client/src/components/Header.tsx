2b69c3e4a70fc0f9d52a14e1454d488b8a718464
Tim Silva <tim17silva@gmail.com>
2023-10-07 22:13:57 -0700

    Font

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { BsHandbag } from "react-icons/bs";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-center h-24 bg-red-200">
+    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-center h-24 text-xl bg-red-200">
       <div className="flex justify-center basis-1/5">
         <span>Thesis</span>
       </div>
