45259b834d28d706468a026080f712b524fb085d
Tim Silva <tim17silva@gmail.com>
2023-10-07 22:15:55 -0700

    Started home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Header />}></Route>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
