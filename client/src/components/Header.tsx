02c2bc73655cbf46bd9128acae17cd3e7d5fdbb1
Tim Silva <tim17silva@gmail.com>
2023-10-11 14:41:53 -0700

    Testing buttons

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Outlet } from "react-router-dom";
 export default function Header() {
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-24 text-xl text-white">
+      <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-24 text-2xl text-white">
         <div className="flex justify-center gap-6 basis-1/5">
           <span>Men</span>
           <span>Women</span>
         </div>
-        <div className="flex justify-center text-2xl basis-1/5">
+        <div className="flex justify-center text-3xl basis-1/5">
           <BsHandbag />
         </div>
       </header>
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section>
       <div className="bg-cover flex justify-center items-center bg-center h-screen w-screen px-10 bg-[url('/public/images/hero.jpg')]">
-        <span className="text-6xl font-thin tracking-widest font-mont">
+        <span className="text-6xl font-thin tracking-widest text-center basis-full font-mont">
           THESIS
         </span>
       </div>
