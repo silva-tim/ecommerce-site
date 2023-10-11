c54b1e16eb6b06c310eec11b738d4815be9abd43
Tim Silva <tim17silva@gmail.com>
2023-10-11 14:30:56 -0700

    Working on homepage

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,19 @@
 import { BsHandbag } from "react-icons/bs";
+import { Outlet } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-center h-24 text-xl bg-red-200">
-      <div className="flex justify-center basis-1/5">
-        <span>Thesis</span>
-      </div>
-      <div className="flex justify-center gap-6 basis-3/5">
-        <span>Men</span>
-        <span>Women</span>
-      </div>
-      <div className="flex justify-center basis-1/5">
-        <BsHandbag />
-      </div>
-    </header>
+    <>
+      <header className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-24 text-xl text-white">
+        <div className="flex justify-center gap-6 basis-1/5">
+          <span>Men</span>
+          <span>Women</span>
+        </div>
+        <div className="flex justify-center text-2xl basis-1/5">
+          <BsHandbag />
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 }
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <div className="bg-cover flex justify-center items-center bg-center h-screen w-screen px-10 bg-[url('/public/images/hero.jpg')]">
+        <span className="text-6xl font-thin tracking-widest font-mont">
+          THESIS
+        </span>
+      </div>
+    </section>
+  );
 }
