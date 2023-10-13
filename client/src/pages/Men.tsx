2cb0c076e79942b0fe29ce2dc9b7c9329b67ec21
Tim Silva <tim17silva@gmail.com>
2023-10-13 11:33:46 -0700

    Working on men's

--- client/src/pages/Men.tsx
+++ client/src/pages/Men.tsx
@@ -1,9 +1,9 @@
 export default function Men() {
   return (
     <section>
-      <div className="bg-cover flex justify-center items-center bg-center h-screen w-screen px-10 bg-[url('/images/men.jpg')]">
-        <span className="text-6xl font-thin tracking-widest text-center text-white basis-full font-mont">
-          Men
+      <div className="bg-cover flex justify-center items-center bg-center h-screen w-screen px-10 bg-[url('/images/m1.jpg')]">
+        <span className="text-6xl font-thin tracking-widest text-center basis-full font-mont">
+          MEN
         </span>
       </div>
       <div className="h-screen bg-white"></div>
