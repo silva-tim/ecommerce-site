ba35d67fbb751e38789bdd618a906347a7807aa3
Tim Silva <tim17silva@gmail.com>
2023-10-12 19:13:12 -0700

    Working on home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
