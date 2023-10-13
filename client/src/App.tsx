b7811dbbac63f519ad6a98a764b973fcb32061ba
Tim Silva <tim17silva@gmail.com>
2023-10-13 11:28:13 -0700

    Starting mens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Men from "./pages/Men";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Men />} />
       </Routes>
     </>
   );
---
+++ client/src/pages/Men.tsx
@@ -0,0 +1,12 @@
+export default function Men() {
+  return (
+    <section>
+      <div className="bg-cover flex justify-center items-center bg-center h-screen w-screen px-10 bg-[url('/images/men.jpg')]">
+        <span className="text-6xl font-thin tracking-widest text-center text-white basis-full font-mont">
+          Men
+        </span>
+      </div>
+      <div className="h-screen bg-white"></div>
+    </section>
+  );
+}
