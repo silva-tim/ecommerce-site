e66dc73f9c6eec0ee64b90c3ffaf86be1fc530c1
Tim Silva <tim17silva@gmail.com>
2023-10-17 15:18:31 -0700

    Added apothecary and women pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Men from "./pages/Men";
 import Layout from "./pages/Layout";
+import Women from "./pages/Women";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
         </Route>
       </Routes>
     </>
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,14 +1,18 @@
-import { BsHandbag } from "react-icons/bs";
+import { BsHandbag, BsHouse } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-24 text-2xl text-white">
-      <div className="flex justify-center gap-6 basis-1/5 font-mont">
-        <span>Men</span>
-        <span>Women</span>
+      <div className="flex justify-center gap-5 basis-1/5 font-mont">
+        <Link to="/men">Men</Link>
+        <Link to="/women">Women</Link>
       </div>
-      <div className="flex justify-center text-3xl basis-1/5">
+      <div className="flex justify-center gap-5 text-3xl basis-1/5">
         <BsHandbag />
+        <Link to="/">
+          <BsHouse />
+        </Link>
       </div>
     </nav>
   );
